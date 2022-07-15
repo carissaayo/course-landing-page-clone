686f01ddcc5a670ea98fd8f5f80e2a0aa97050bf
Yussuf <you@example.com>
2022-07-15 12:29:46 +0100

    working on service-main-con

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
 import Header from "./components/Header/Header";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Services/>
     </div>
   );
 }
