b0479c6fa015fe57b7662c07f021c599fa496d56
Yussuf <you@example.com>
2022-07-15 11:03:10 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "../../assets/img/logo.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <section className="nav flex">
+      <div className="logo-con">
+        <a href="#">
+        <img src={logo} alt="logo" />
+        </a>
+      </div>
+      <nav className="nav-con flex">
+        <ul className="nav__links flex">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Courses</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#"> Blog</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <ul className="sign-con flex">
+          <li>
+            <a href="#">Sign</a>
+          </li>
+          <li>
+            <a href="#">Log In</a>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
