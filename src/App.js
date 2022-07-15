a96b68dd7e807cbfab6f102683e81e583ffefd98
Yussuf <you@example.com>
2022-07-15 11:43:18 +0100

    done with the hero basic styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <h1>hello</h1>
+     <Header/>
     </div>
   );
 }
